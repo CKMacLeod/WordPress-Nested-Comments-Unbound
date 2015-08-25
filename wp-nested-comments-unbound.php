31cc71918c0eb44c420538bf41f5296379d25e0f
CKMacLeod <ckm@ckmacleod.com>
2015-08-25 13:47:41 -0700

    Includes Styles for "Snaking" Comment Thread
    The  "Styles for Snaking Version" can be un-commented to produce comment
    threads that "bounce" from margin to margin. Tested in 2015 and a
    "ul"-based comment threads.

--- wp-nested-comments-unbound.php
+++ wp-nested-comments-unbound.php
@@ -2,9 +2,9 @@
 
 /**
  * Plugin Name: WP Nested Comments Unbound
- * Description: Allows setting nested comments thread to any depth
+ * Description: Allows setting nested comments thread to any depth and creating "unbounded" nested threads
  * Plugin URI: http://www.ckmacleod.com/plugins/bigger-max-depth
- * Version:     1.0
+ * Version:     1.1
  * Author:      CK MacLeod
  * Author URI:  http://www.ckmacleod.com/
  * License:     GPL
@@ -21,6 +21,7 @@ function big_max_depth() {
     $new_max_depth = 999;
 
     return $new_max_depth;
+
                 }
 
 add_filter('thread_comments_depth_max','big_max_depth');
@@ -28,27 +29,47 @@ add_filter('thread_comments_depth_max','big_max_depth');
 
 }
 
-//adds "super-max" comment class based on "breakpoint" depth - 
+//adds "super-max," "breakpoint," "leftturn" and "rightturn" classes
 
 function bmd_comment_class( $classes ) {
     
     global $comment_depth;
     
-    $breakpoint = 10;
+    $breakpoint = 5;
     
-    if ($comment_depth > $breakpoint ) {
+    if ($comment_depth >= $breakpoint ) {
 
+//super-max styles all comments higher than the initial breakpoint	
+	
         $classes[] = 'super-max';
+        
+            if ( ( $comment_depth % $breakpoint == 0) ){
+        
+//breakpoint is added to comments at the specified breakpoint		
+		
+        $classes[] = 'breakpoint';
+    }
+        
+    if ($comment_depth > $breakpoint) {
+        
+        if ( ceil(($comment_depth/$breakpoint)) % 2 == 0) {
+		
+//leftturn and righturn set styles for comments between breakpoints (e.g. for snaking effect)
+            
+        $classes[] = 'leftturn'; } 
+		
+		else {
+            
+        $classes[] = 'rightturn';
+        
+		}
+    
+	}
 
         }
-        
-    if ( ( $comment_depth % $breakpoint == 0) ){
-        
-        $classes[] = 'breakpoint';
-        
-    } 
 	
-        return $classes;
+    return $classes;
+
 }
 
 add_filter('comment_class','bmd_comment_class');
